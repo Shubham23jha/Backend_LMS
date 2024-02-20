3e62ce5f156dc05ef90086aa90b4593ab250de6a
Shubham23jha <skj23072002@gmail.com>
2024-02-20 15:42:19 +0530

    Add cookie options for dev environment

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -12,8 +12,8 @@ const cookieOptions = {
   secure: process.env.NODE_ENV === 'production' ? true : false,
   maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
   httpOnly: true,
-  sameSite: 'none',
-  credentials: true,
+  sameSite: process.env.NODE_ENV === 'production' ? 'none': null,
+  credentials: process.env.NODE_ENV === 'production' ? true: null,
 };
 
 
